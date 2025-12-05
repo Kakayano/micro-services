ec380ea61c0018b36218e54c2868c2a5517e33f3
Kakayano <neo.herbigniaux@std.heh.be>
2025-12-05 09:12:29 +0100

    feat: add of mongodb to product-service

---
+++ product-service/src/main/java/org/example/productservice/persistence/ProductEntity.java
@@ -0,0 +1,13 @@
+package org.example.productservice.persistence;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public record ProductEntity(
+        @Id
+        String id,
+        int productId,
+        String name,
+        int weight
+) {}
